e0ec5de3bee48e7d7f9ec7e1eef8823f6024b56f
BookCnk <bookchanakarn@gmail.com>
2023-12-07 03:18:35 +0700

    first commit

--- INT201/main/quiz/module 3/paginateManagement.js
+++ INT201/main/quiz/module 3/paginateManagement.js
@@ -1,163 +1,70 @@
-
-// npm test CommonJS
 // const { template } = require('@babel/core')
-const { getItemsOfCurrentPage, getTotalPages } = require('./lib/paginate.js')
-const products = require('./data/products.js')
-
-// browser ES module
 // import { getItemsOfCurrentPage, getTotalPages } from './lib/paginate.js'
 // import { products } from './data/products.js'
 
-function paginateManagement(items, rows) {
+const { getItemsOfCurrentPage, getTotalPages } = require("./lib/paginate.js");
+const products = require("./data/products.js");
 
-  const rowsPerPage = rows
-  const products = items
+//64130500034, Tanasit Aintakhan
+function paginateManagement(items, rows) {
+  const rowsPerPage = rows;
+  const products = items;
 
   const showItemsOfCurrentPage = (currentPageNumber) => {
-    const ItemsOfCurrent = getItemsOfCurrentPage(
-        products,
-        currentPageNumber,
-        rowsPerPage
-      ) // return array ของหน้านั้น
+    const itemsOfCurrentPage = getItemsOfCurrentPage(products, currentPageNumber, rowsPerPage);
+    const ulElement = document.getElementById("products");
 
-    const productsUl = document.getElementById('products')
-    ItemsOfCurrent.forEach((item)=>{
-      const liElement = document.createElement('li')
-      liElement.textContent = `ID:${item.id}, NAME:${item.name}`
-      productsUl.appendChild(liElement)
-    })
-  }
+    itemsOfCurrentPage.forEach((item) => {
+      const liElement = document.createElement("li");
+      liElement.textContent = `ID:${item.id}, NAME:${item.name}`;
+      ulElement.appendChild(liElement);
+    });
+  };
 
   const pageHandler = (event) => {
-    const productsUl = document.getElementById('products')
-    productsUl.textContent=''
+    const divPagination = document.querySelector(".pagination");
+    const buttons = divPagination.querySelectorAll("button");
 
-    const pageStyle = document.querySelectorAll('button')
-    pageStyle.forEach((page)=>(page.style = 'border: 1px solid #999'))
+    buttons.forEach((button) => {
+      button.style.border = "1px solid #999";
+      button.style.backgroundColor = "#fff";
+    });
 
-    const currentPage = event?.target.id ?? 1
-    console.log(currentPage)
-    showItemsOfCurrentPage(currentPage)
+    if (event === undefined) {
+      showItemsOfCurrentPage(1);
+      buttons[0].style.backgroundColor = "LightSteelBlue";
+    } else {
+      const currentPage = event.target.id;
+      const ulElement = document.getElementById("products");
+      ulElement.textContent = "";
+      showItemsOfCurrentPage(currentPage);
+      event.target.style.backgroundColor = "LightSteelBlue";
+    }
 
-    // ค้นหาปุ่มที่มี id เท่ากับค่าของ currentPage และเก็บ reference ไว้ใน targetPageButton.
-    const targetPageButton = document.getElementById(currentPage)
-    //กำหนดสีพื้นหลังของปุ่มที่มี id เท่ากับ currentPage เป็น LightSteelBlue. 
-    //เป็นการhighlight ปุ่มที่เลือกอยู่เพื่อแสดงว่าเป็นหน้าปัจจุบัน.
-    targetPageButton.style = 'background-color: LightSteelBlue'
-  }
+    buttons.forEach((button) => {
+      button.addEventListener("click", pageHandler); 
+    });
+  };
 
   const showPageNumbers = () => {
-    const totalPages = getTotalPages(products, rowsPerPage) // return button will create
+    const totalPages = getTotalPages(products, rowsPerPage);
+    const divPagination = document.querySelector(".pagination");
 
-    const paginateDiv = document.querySelector('.pagination')
-    for (let page = 1; page <= totalPages; page++) {
-      const buttonPage = document.createElement('button')
-      buttonPage.textContent = page
-      buttonPage.setAttribute('id', page)
-      paginateDiv.appendChild(buttonPage)
-      buttonPage.addEventListener('click', pageHandler) // add event
+    for (let i = 1; i <= totalPages; i++) {
+      const button = document.createElement("button");
+      button.textContent = i;
+      button.setAttribute('id', i);
+      divPagination.appendChild(button);
     }
-  }
+  };
 
-return {
-  showPageNumbers,
-  pageHandler
-  }
+  return {
+    showPageNumbers,
+    pageHandler,
+  };
 }
-
-
-// npm test CommonJS
-module.exports = paginateManagement
-
-// browser ES module
+module.exports = paginateManagement;
 // export { paginateManagement }
-// const { showPageNumbers,  pageHandler } = paginateManagement(products, 10)
-// showPageNumbers()
-// pageHandler()
-
-
-
-
-
-
-
-
-
-// function paginateManagement(items, rows) {
-//     const products = items
-//     const rowsPerPage = rows
-
-//     let showPageNumber = () => {
-//         let totalPage = getTotalPages(products, rowsPerPage);
-//         let divPagination = document.querySelector(".pagination");
-
-//         for (let page = 1; page <= totalPage; page++) {
-//             let createButtom = document.createElement("button")
-//             createButtom.id = page
-//             createButtom.textContent = page
-//             divPagination.appendChild(createButtom)
-
-//             createButtom.addEventListener("click", pageHandler)
-//         }
-//     }
-
-//     let showItemsOfCurrentPage = (currentPageNumber) => {
-//         let items = getItemsOfCurrentPage(products, currentPageNumber, rowsPerPage)
-//         let ulElement = document.getElementById("products")
-//         ulElement.innerHTML = ""
-//         for (const item of items) {
-//             let createLi = document.createElement("li");
-//             createLi.textContent = `ID:${item.id}, NAME:${item.name}`
-//             ulElement.appendChild(createLi)
-//         }
-//     }
-
-//     // let pageHandler = (event) => {
-//     //     let totalPage = getTotalPages(products, rowsPerPage);
-    
-//     //     showItemsOfCurrentPage(event.target.id )
-//     //     for (let page = 1; page <= totalPage; page++) {
-//     //         let btn = document.getElementById(page)
-//     //         btn.style.removeProperty("background-color")
-//     //         btn.style.removeProperty("border")
-//     //     }
-//     //     event.target.style.backgroundColor = "LightSteelBlue"
-//     //     event.target.style.border = "1px solid #999"
-//     // }
-
-//     const pageHandler = (event) => {
-//         const productsUsee = document.getElementById('products')
-//         productsUsee.textContent = '' // remove previous page’s items
-    
-//         const pageColor = document.querySelectorAll('button')
-    
-//         pageColor.forEach((page) =>(
-//           page.style = 'border: 1px solid #999' // remove stylesheet of previous page button and assign to original button stylesheet 'border: 1px solid #999'
-//         ))
-    
-//         const currentPage = event?.target.id ?? 1
-//         // event?.target.id จะใช้เพื่อดึงค่า id ของ Element ใน event , ? 
-//         // ?? จะใช้เพื่อตรวจสอบว่าค่าที่ได้มีค่าหรือไม่ หากมีค่าก็จะใช้ค่านั้น หากไม่มีค่าก็จะใช้ค่าที่สองที่กำหนดไว้
-    
-    
-    
-//         showItemsOfCurrentPage(currentPage)
-//         // adding stylesheet 'background-color: LightSteelBlue'
-//         const targetPageButton = document.getElementById(currentPage)
-//         targetPageButton.style = 'background-color: LightSteelBlue'
-    
-        
-//       }
-
-//     showItemsOfCurrentPage()
-//     showPageNumber()
-// }
-
-// // npm test CommonJS
-// // module.exports = paginateManagement
-
-// browser ES module
-// export { paginateManagement }
-// const { showPageNumbers,  pageHandler } = paginateManagement(products, 10)
+// const { showPageNumbers, pageHandler } = paginateManagement(products, 10)
 // showPageNumbers()
 // pageHandler()
